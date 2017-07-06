231c985e2795cf9499ccc5a5c13e1037f9616210
Patrick Kao <patrick.d.kao@gmail.com>
2017-07-06 14:52:31 -0700

    Changed Script Location References
    Should work on any computer as soon as github repo is downloaded

--- startVisOdom.bat
+++ startVisOdom.bat
@@ -1,6 +1,7 @@
-roslaunch rtabmap_ros visodomlong.launch &
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+roslaunch $DIR/visodomlong.launch &
 sleep 5
 #rosservice call /rtabmap/rgbd_odometry/set_logger_level ros WARN
 echo printing suppressed
-source ~/Desktop/topicforwarding/devel/setup.bash
+source $DIR/topicforwarding/devel/setup.bash
 rosrun odomtopose forwardotp
